d7d98c653621fab3c340bf4fefda994d9a8233d4
samoeun chhay <samoeunchh@gmail.com>
2021-10-13 09:34:12 +0700

    Add project files.

---
+++ SR78POS/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SR78POS.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
