682d503ed222486fa7b2fbc4e088bf232e6968c0
samoeun chhay <samoeunchh@gmail.com>
2021-10-20 09:13:01 +0700

    Add unit

--- SR78POS/Data/ApplicationDbContext.cs
+++ SR78POS/Data/ApplicationDbContext.cs
@@ -14,5 +14,6 @@ namespace SR78POS.Data
         {
         }
         public DbSet<Category> Category { get; set; }
+        public DbSet<Unit> Unit { get; set; }
     }
 }
---
+++ SR78POS/Models/Unit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SR78POS.Models
+{
+    public class Unit
+    {
+        [Key]
+        public Guid UnitId { get; set; }
+        [Required]
+        [Display(Name ="Unit Name")]
+        [MaxLength(50)]
+        public string UnitName { get; set; }
+        public int Qty { get; set; }
+    }
+}
