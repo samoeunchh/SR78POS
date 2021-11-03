e18d29ebf6861631e2ac4f36fa8898531694e1ed
samoeun chhay <samoeunchh@gmail.com>
2021-11-03 09:34:48 +0700

    Barcode Scan

---
+++ SR78POS/Controllers/ProductController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SR78POS.Data;
+using SR78POS.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SR78POS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public ProductController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        // GET: api/<ProductController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<ProductController>/5
+        [HttpGet("{code}")]
+        public async Task<Product> Get(string code)
+            =>await _context.Product.FirstOrDefaultAsync(x => x.Barcode.Equals(code));
+
+        // POST api/<ProductController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ProductController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ProductController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
