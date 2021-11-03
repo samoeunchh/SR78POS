bda61757e9eb2b977559ae0127f15e7eb665976f
samoeun chhay <samoeunchh@gmail.com>
2021-11-03 10:03:54 +0700

    Add Unit for sale

--- SR78POS/wwwroot/js/site.js
+++ SR78POS/wwwroot/js/site.js
@@ -2,3 +2,19 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+var options = "";
+$.ajax({
+    method: "GET",
+    contentType: "application/json",
+    url: "/Products/GetUnits",
+    success: function (v) {
+        console.log(v);
+        $.each(v, function (i, v1) {
+            console.log(v1);
+            options += "<option value='" + v1.unitId + "'>" + v1.unitName + "</option>";
+        });
+    },
+    error: function (error) {
+        console.log(error)
+    }
+});
